cab551d46bf2f3cddc6c2d6c0b0ca6293fb539d1
ninesky00 <jiangzho@gmail.com>
2021-02-25 01:02:30 -0600

    remove comment

--- app/controllers/api/v1/usages_controller.rb
+++ app/controllers/api/v1/usages_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::UsagesController < ApplicationController
   def show
     render json: UsagesSerializer.new(UsagesFacade.utility_info(params[:id]))
-    #usage = Usage.new(usage_params)
   end
 end
