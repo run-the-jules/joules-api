ef12655ed249a4e82e8960c494f2360d5800f1a2
Aiden Mendez <aidenmendez@gmail.com>
2021-02-26 18:36:08 -0700

    Ignore cassettes and complete provider spec

--- spec/requests/api/v1/providers_request_spec.rb
+++ spec/requests/api/v1/providers_request_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "providers api endpoint", type: :request do
-  describe "(happy path)" do
-    it "returns a list of all available providers", :vcr do
-      
-      providers_stub = File.read("spec/fixtures/providers_data.json")
-      stub_request(:get, "https://mysterious-ravine-12345.herokuapp.com/utilities").to_return(status: 200, body: providers_stub)
+  describe "(happy path)", :vcr do
+    it "returns a list of all available providers" do
 
       get "/api/v1/providers"
       expect(response.status).to eq(200)
 
-      json = json.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+      json = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(json.count).to eq(29)
+      expect(json.first).to have_key(:id)
+      expect(json.first[:attributes]).to have_key(:name)
+      expect(json.first[:attributes]).to have_key(:id)
     end
   end
 end
