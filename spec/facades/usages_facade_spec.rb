3ea4db71bdd5a3fdc04e3cb90ba3b92d1fe458ac
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-27 22:00:38 -0700

    need to fix up one test

--- spec/facades/usages_facade_spec.rb
+++ spec/facades/usages_facade_spec.rb
@@ -4,11 +4,11 @@ describe UsagesFacade do
   describe 'utility_info' do
     it 'brings user utility information' do
       usage_stub = File.read("spec/fixtures/usage_data.json")
-      stub_request(:get, "https://mysterious-ravine-12345.herokuapp.com/api/v1/users/1/usage").
+      stub_request(:get, "https://joules-microservice.herokuapp.com/api/v1/users/1/usage").
       to_return(status: 200, body: usage_stub)
 
       id = 1
-      usage = UsagesFacade.utility_info(id)
+      usage = MeterActivationFacade.utility_info(id)
       expect(usage).to be_a(Usage)
       expect(usage.user_id).to be_a(Numeric)
       expect(usage.kwh).to be_a(Numeric)
