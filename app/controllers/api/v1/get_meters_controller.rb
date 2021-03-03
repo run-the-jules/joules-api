4aebe00a391558b3a0d19fff9ecc471578f3ffda
Robert Heath <robert.b.heathii@gmail.com>
2021-03-02 19:28:19 -0600

    Update meters controller for sad path

--- app/controllers/api/v1/get_meters_controller.rb
+++ app/controllers/api/v1/get_meters_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::GetMetersController < ApplicationController
   def index
-    render json: MeterActivationFacade.referral(params)
+    if params[:referral].to_i != 0
+      render json: MeterActivationFacade.referral(params)
+    else
+      raise_not_found!
+    end
   end
 end
