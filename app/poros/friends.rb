b1887ba3db2e3bf90399ff8889088c4c271ef7e7
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-03 20:34:08 -0700

    added end date for user

--- app/poros/friends.rb
+++ app/poros/friends.rb
@@ -1,9 +1,10 @@
 class Friends
   #better class name, possibly
-  attr_reader :user_id, :user_kwh, :friends_data
+  attr_reader :user_id, :user_kwh, :end_date, :friends_data
   def initialize(data)
     @user_id = data[:user_id]
     @user_kwh = data[:user_kwh]
+    @end_date = data[:end_date]
     @friends_data = data[:friends]
   end
 end
